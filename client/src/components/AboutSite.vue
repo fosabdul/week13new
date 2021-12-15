4172d1263c9619e400df40de6d06fddd550ac611
fosabdul <um0364nu@go.minnstate.edu>
2021-12-14 20:50:11 -0600

    my name added

--- client/src/components/AboutSite.vue
+++ client/src/components/AboutSite.vue
@@ -3,7 +3,7 @@
     <h2>Where have you traveled to?</h2>
     
     <p>Use this site to keep track of how many states you have visited</p>
-    <p>A Vue.js site by (fill in your name here)</p>
+    <p>A Vue.js site by Fosiya</p>
   </div>
 </template>
 
